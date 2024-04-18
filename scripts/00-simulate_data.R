4479803e4559a8b907b2c2570319f3d909c7d93e
Bernice Bao <bernice.bao@mail.utoronto.ca>
2024-04-18 03:41:16 +0000

    add simulate data

--- scripts/00-simulate_data.R
+++ scripts/00-simulate_data.R
@@ -36,3 +36,24 @@ head(simulated_data)
 #### Save data ####
 write_csv(simulated_data,"inputs/data/simulated_data.csv")
 
+#### Simulate final data ####
+set.seed(85)
+
+simulated_data <-
+  tibble(building_number = c(1:100),
+         has_wifi = sample(c("Y","N"), size = 100, replace = TRUE, prob = c(0.9, 0.1)),
+         # Based on: https://www.toronto.ca/city-government/accountability-operations-customer-service/long-term-vision-plans-and-strategies/smart-cityto/internet-connectivity-connectto/connectto-free-public-wifi-locations/
+         building_type = c(
+           rep(x = "library", times = 20),
+           rep(x = "Arena", times = 40),
+           rep(x = "Community", times = 40)
+         ),
+         ward = rep(c(x = "Etobicoke North", "Etobicoke Centre", "Etobicoke-Lakeshore", "Parkdale-High Park", "York South-Weston", "York Centre", "Humber River-Black Creek", "Eglinton-Lawrence", "Davenport", "Spadina-Fort York", "University-Rosedale", "Toronto-St. Paul's", "Toronto Centre", "Toronto-Danforth", "Don Valley West", "Don Valley East", "Don Valley North", "Willowdale", "Beaches-East York", "Scarborough Southwest", "Scarborough Centre", "Scarborough-Agincourt", "Scarborough North", "Scarborough-Guildwood", "Scarborough-Rouge Park"), times = 4),
+         ward_number =rep(c(1:25), times = 4)
+  )
+
+head(simulated_data)
+
+#### Save final data ####
+write_csv(simulated_data,"inputs/data/simulated_data.csv")
+
