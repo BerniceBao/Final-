9aae25065e1157d8f10d76e35dc486cd19cdc1d6
Bernice Bao <bernice.bao@mail.utoronto.ca>
2024-04-18 04:08:00 +0000

    Delete unnecessary model

--- scripts/04-model.R
+++ scripts/04-model.R
@@ -14,24 +14,11 @@ library(modelsummary)
 #### Read data ####
 final_data <- read.csv(here::here("outputs/data/final_data.csv"))
 
-#### Model data 1 ####
-wifi_model <-
-  stan_glm(
-    formula = Number_of_free_wifi ~ Population + Income,
-    data = final_data,
-    family = gaussian(),
-    prior = normal(location = 0, scale = 2.5, autoscale = TRUE),
-    prior_intercept = normal(location = 0, scale = 2.5, autoscale = TRUE),
-    prior_aux = exponential(rate = 1, autoscale = TRUE),
-    seed = 123
-  )
-
-
 ## creating simulated data for the model
 set.seed(853)
 
 
-#### Linear Model data 2 ####
+#### Linear Model data 1 ####
 population_model <-
   lm(
     Number_of_free_wifi ~ Population,
@@ -42,7 +29,7 @@ summary(population_model)
 modelsummary(population_model)
 
 
-#### Linear Model data 3 ####
+#### Linear Model data 2 ####
 income_model <-
   lm(
     Number_of_free_wifi ~ Income,
@@ -54,18 +41,13 @@ modelsummary(income_model)
 
 #### Save model ####
 
-saveRDS(
-  wifi_model,
-  file = here::here("outputs/models/wifi_model.rds")
-)
-
-# LINEAR MODEL 2
+# LINEAR MODEL 1
 saveRDS(
   population_model,
   file = "outputs/models/population_model.rds"
 )
 
-# LINEAR MODEL 3
+# LINEAR MODEL 2
 saveRDS(
   income_model,
   file = "outputs/models/income_model.rds"
